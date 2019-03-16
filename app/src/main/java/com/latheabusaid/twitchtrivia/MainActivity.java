54fa65f5effd091f98888a4a2f723fac2cc6719e
Lathe Abusaid <labusaid@gmail.com>
2019-03-16 02:21:04 -0500

    created basic UI layout and dialog box

--- app/src/main/java/com/latheabusaid/twitchtrivia/MainActivity.java
+++ app/src/main/java/com/latheabusaid/twitchtrivia/MainActivity.java
@@ -1,13 +1,63 @@
 package com.latheabusaid.twitchtrivia;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String m_Text = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void nameButtonClicked(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Title");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    public void readyButtonClicked(View view) {
+    }
+
+    public void answer1Clicked(View view) {
+    }
+
+    public void answer2Clicked(View view) {
+    }
+
+    public void answer3Clicked(View view) {
+    }
+
+    public void answer4Clicked(View view) {
     }
 }
