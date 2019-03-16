4d66338f089673cfb925c660dbb6ad5580bee28a
Lathe Abusaid <labusaid@gmail.com>
2019-03-16 03:25:30 -0500

    got requests working

--- app/src/main/java/com/latheabusaid/twitchtrivia/MainActivity.java
+++ app/src/main/java/com/latheabusaid/twitchtrivia/MainActivity.java
@@ -1,34 +1,39 @@
 package com.latheabusaid.twitchtrivia;
 
-import android.app.DownloadManager;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "TwitchTrivia";
 
+    String siteUrl = "https://twitch-trivia.herokuapp.com";
+    String currentQ = "no question found";
     private String m_Text = "";
 
     OkHttpClient client = new OkHttpClient();
 
-    String getFromUrl(String url) throws IOException {
+    Call getFromUrl(String url, Callback callback) throws IOException {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
 
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
     }
 
 
@@ -54,7 +59,38 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 m_Text = input.getText().toString();
 //                get request @url/game/start?name=m_Text
+                try {
+                    getFromUrl(siteUrl + "/game/start?name=" + m_Text, new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            Log.d(TAG, "onResponse: game started");
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 //                get request @url/game/question/current
+                try {
+                    getFromUrl(siteUrl + "/game/question/current", new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            currentQ = response.body().string();
+                            Log.d(TAG, "onResponse: " + currentQ);
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -67,8 +103,19 @@ public class MainActivity extends AppCompatActivity {
         builder.show();
     }
 
-    public void readyButtonClicked(View view) {
+    public void readyButtonClicked(View view) throws IOException {
 //        get request @url/game/question/ready
+        getFromUrl(siteUrl + "/game/question/ready", new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
     }
 
     public void answer1Clicked(View view) {
